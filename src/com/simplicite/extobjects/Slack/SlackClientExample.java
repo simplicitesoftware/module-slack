c3c02d3e48e8336279710919e36f8b2127459c4f
designer (Designer) <no-reply@simplicite.fr>
2020-01-15 13:22:31 +0100

    Milestone

--- src/com/simplicite/extobjects/Slack/SlackClientExample.java
+++ src/com/simplicite/extobjects/Slack/SlackClientExample.java
@@ -1,27 +1,29 @@
 package com.simplicite.extobjects.Slack;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.simplicite.util.AppLog;
 import com.simplicite.util.exceptions.APIException;
 import com.simplicite.util.tools.Parameters;
-//import com.simplicite.util.tools.SlackTool;
+import com.simplicite.util.tools.SlackTool;
 
 /**
  * Slack client example
  */
-public class SlackClientExample extends com.simplicite.util.ExternalObject {
+public class SlackClientExample extends com.simplicite.webapp.web.ResponsiveExternalObject {
 	private static final long serialVersionUID = 1L;
 	
 	@Override
-	public Object display(Parameters params) {
+	public Object service(Parameters params) {
 		try {
-			setHTML(getDisplay());
-		} catch (Exception e) {
-			AppLog.error(getClass(), "display", null, e, getGrant());
-			setHTML("Error: " + e.getMessage());
+			String message = params.getParameter("message");
+			AppLog.info(getClass(), "service", "Posting to Slack, message: " + message, getGrant());
+			SlackTool st = new SlackTool(getGrant());
+			st.postMessage("demo", message);
+			return new JSONObject().put("message", message).put("status", 200);
+		} catch (APIException e) {
+			AppLog.error(getClass(), "service", null, e, getGrant());
+			return null;
 		}
-		return javascript("void(0);");
 	}
 }
